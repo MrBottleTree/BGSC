62577588ea55d2b5262e54695872fbbd2ddcd4ab
Vishrut Ramraj <65703492+MrBottleTree@users.noreply.github.com>
2025-08-30 02:45:14 +0530

    awd

--- bgsc/settings.py
+++ bgsc/settings.py
@@ -136,3 +136,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login/"
 LOGIN_URL = "/login/"
+STATIC_ROOT = BASE_DIR / "static_collected"
+MEDIA_ROOT = BASE_DIR / "media"
